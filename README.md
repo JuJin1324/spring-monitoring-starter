63c09d5a8208ace400947f4b5d8aa1228229f622
JuJin1324 <jujin@100fac.com>
2022-12-13 14:47:52 +0900

    init project

---
+++ README.md
@@ -0,0 +1,14 @@
+# spring-monitoring-starter
+
+## Spring Actuator
+### info.env 설정
+> `GET /actuator/info` 에 프로젝트 빌드 정보 표시하기  
+> build.gradle
+> ```groovy
+> ...
+> springBoot {
+>     buildInfo()
+> }
+> ...
+> 
+> ```
---
+++ src/main/java/starter/spring/monitoring/SpringMonitoringStarterApplication.java
@@ -0,0 +1,13 @@
+package starter.spring.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonitoringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMonitoringStarterApplication.class, args);
+    }
+
+}
